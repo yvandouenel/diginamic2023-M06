f2e1dae6e44897d17e36b2e3b9f8ef4aa16c3268
yvandouenel <y.douenel@coopernet.fr>
2024-01-23 14:12:03 +0100

    Exo Convertisseur corrigé.

--- TypeScript/exercices/convertisseur/src/index.ts
+++ TypeScript/exercices/convertisseur/src/index.ts
@@ -13,12 +13,13 @@ const inputEuro: HTMLInputElement = createMarkup("input", "", form, {
   id: "euro",
 }) as HTMLInputElement;
 
-inputEuro.addEventListener("input", (e: Event)=>{
+inputEuro.addEventListener("input", (e: Event) => {
   // Comment typer value de manière à imposer un nombre ?
-  const value: number = inputEuro.value;
+  const value: number = parseFloat(inputEuro.value);
   console.log(`Input Euro :`, value);
 
   // Il faut maintenant modifier la value de l'inputChf en fonction du taux de conversion (1.06) et de value
+  inputChf.value = (value / 1.06).toFixed(2);
 });
 
 const labelEuro: HTMLElement = createMarkup("label", "euros", form, {
@@ -29,4 +30,11 @@ const inputChf: HTMLInputElement = createMarkup("input", "euros", form, {
   type: "text",
 }) as HTMLInputElement;
 
+inputChf.addEventListener("input", (e: Event) => {
+  // Comment typer value de manière à imposer un nombre ?
+  const value: number = parseFloat(inputChf.value);
+  console.log(`Input Chf :`, value);
 
+  // Il faut maintenant modifier la value de l'inputChf en fonction du taux de conversion (1.06) et de value
+  inputEuro.value = (value * 1.06).toFixed(2);
+});
