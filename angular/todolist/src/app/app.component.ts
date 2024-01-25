7ad994935710b4c43ee0a9f42e82fb98ba01e320
yvandouenel <y.douenel@coopernet.fr>
2024-01-25 09:54:45 +0100

    Tests de départ pour apprendre les bases d'Angular

--- angular/todolist/src/app/app.component.ts
+++ angular/todolist/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TaskInterface } from './shared/interfaces/task.interface';
+import { TasksListComponent } from './tasks-list/tasks-list.component';
 
 @Component({
   selector: 'digi-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule],
+  imports: [RouterOutlet, CommonModule, FormsModule, TasksListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ angular/todolist/src/app/tasks-list/tasks-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { TaskInterface } from '../shared/interfaces/task.interface';
+
+@Component({
+  selector: 'digi-tasks-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './tasks-list.component.html',
+  styleUrl: './tasks-list.component.css'
+})
+export class TasksListComponent {
+  
+}
