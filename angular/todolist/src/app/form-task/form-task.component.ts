661dd61a303b8d556a5514c8740773cd1f96eb85
yvandouenel <y.douenel@coopernet.fr>
2024-01-30 14:57:47 +0100

    reset du formulaire.

--- angular/todolist/src/app/form-task/form-task.component.ts
+++ angular/todolist/src/app/form-task/form-task.component.ts
@@ -1,21 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { DataTasksService } from './../data-tasks.service';
 
 @Component({
   selector: 'digi-form-task',
   standalone: true,
   imports: [CommonModule, FormsModule],
-  
-templateUrl: './form-task.component.html',
+
+  templateUrl: './form-task.component.html',
   styleUrl: './form-task.component.css',
 })
 export class FormTaskComponent {
   // Injection de dépendance
-  constructor(private dataTasksService: DataTasksService){}
-  register(taskTitle: string) {
-    console.log(`Formulaire soumis`, taskTitle);
-    this.dataTasksService.setFormValues(taskTitle);
+  constructor(private dataTasksService: DataTasksService) {}
+  register(taskForm: NgForm) {
+    console.log(`Formulaire soumis`, taskForm.value);
+    this.dataTasksService.setFormValues(taskForm.value);
+    taskForm.resetForm();
   }
 }
